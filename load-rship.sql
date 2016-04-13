a9266117bf0d1f00a53578d4ef12fed7eead2b84
Vince Wilding <GitHub@WincingDevil.com>
2016-04-13 08:53:06 -0400

    Update load-rship.sql
    Added 'N/A'

--- load-rship.sql
+++ load-rship.sql
@@ -6,4 +6,4 @@ INSERT INTO relationship (relationship) VALUES ('skos:narrower');
 INSERT INTO relationship (relationship) VALUES ('isMentionedIn');
 INSERT INTO relationship (relationship) VALUES ('hasAnalysisTool');
 INSERT INTO relationship (relationship) VALUES ('hasCaseStudy');
-
+INSERT INTO relationship (relationship) VALUES ('N/A');
