bb782b5ee25d4e680a843f61d6a455d19aad81a2
Vince Wilding <GitHub@WincingDevil.com>
2016-04-13 08:51:48 -0400

    Updated Driver and new load term map gcis files
    in load-term-map-gcis:
    Mosquitoes vs Mosquito
    Deleted second Septicemic Plague (duplicated)

--- driver.sql
+++ driver.sql
@@ -1,5 +1,4 @@
--- make sure that you do a "hypnotoad -s ~/gcis/bin/tuba" to stop the server
-
+\! echo make sure that you do a hypnotoad -s ~/gcis/bin/tuba to stop the server
 \connect postgres
 DROP DATABASE gcis;
 CREATE DATABASE gcis WITH TEMPLATE = template0 ENCODING = 'SQL_ASCII' LC_COLLATE = 'C' LC_CTYPE = 'C';
@@ -9,9 +8,17 @@ ALTER DATABASE gcis OWNER TO postgres;
 \i 2190_context_relationship_term.sql
 insert into lexicon values ('cdi', 'Climate Data Initiative');
 insert into context (lexicon_identifier, identifier) values ('cdi', 'theme');
+\! echo load-term
 \i load-term.sql
+\! echo load relationship
 \i load-rship.sql
+\! echo load cmap
 \i load-cmap.sql
+\! echo load term-map
 \i load-term-map.sql
+\! echo load term-map-crt
 \i load-term-map-crt.sql
+\! echo load term-map-GCIS
+\i load-term-map-gcis.sql
+\! echo Following must be LAST file loaded
 \i 2200_convert_uuid_to_varchar.sql
